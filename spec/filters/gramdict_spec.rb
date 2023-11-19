aed9efb2978b01579c407578ac5f6d04c5e614ee
aaronabraham311 <aaronabraham311@gmail.com>
2023-11-19 09:45:41 +0000

    feat: adding tests

--- spec/filters/gramdict_spec.rb
+++ spec/filters/gramdict_spec.rb
@@ -28,4 +28,52 @@ describe GramDict do
       expect(format_regex).to match('<date> <time> <message>')
     end
   end
+
+  describe '#single_gram_upload' do
+    let(:gram) { 'example' }
+
+    it 'correctly updates the single gram count' do
+      expect { subject.single_gram_upload(gram) }
+        .to change { subject.instance_variable_get(:@single_gram_dict)[gram] }.from(nil).to(1)
+
+      expect { subject.single_gram_upload(gram) }
+        .to change { subject.instance_variable_get(:@single_gram_dict)[gram] }.from(1).to(2)
+    end
+  end
+
+  describe '#double_gram_upload' do
+    let(:double_gram) { 'example gram' }
+
+    it 'correctly updates the double gram count' do
+      expect { subject.double_gram_upload(double_gram) }
+        .to change { subject.instance_variable_get(:@double_gram_dict)[double_gram] }.from(nil).to(1)
+
+      expect { subject.double_gram_upload(double_gram) }
+        .to change { subject.instance_variable_get(:@double_gram_dict)[double_gram] }.from(1).to(2)
+    end
+  end
+
+  describe '#tri_gram_upload' do
+    let(:tri_gram) { 'example tri gram' }
+
+    it 'correctly updates the tri gram count' do
+      expect { subject.tri_gram_upload(tri_gram) }
+        .to change { subject.instance_variable_get(:@tri_gram_dict)[tri_gram] }.from(nil).to(1)
+
+      expect { subject.tri_gram_upload(tri_gram) }
+        .to change { subject.instance_variable_get(:@tri_gram_dict)[tri_gram] }.from(1).to(2)
+    end
+  end
+
+  describe '#four_gram_upload' do
+    let(:four_gram) { 'example four gram' }
+
+    it 'correctly updates the four gram count' do
+      expect { subject.four_gram_upload(four_gram) }
+        .to change { subject.instance_variable_get(:@four_gram_dict)[four_gram] }.from(nil).to(1)
+
+      expect { subject.four_gram_upload(four_gram) }
+        .to change { subject.instance_variable_get(:@four_gram_dict)[four_gram] }.from(1).to(2)
+    end
+  end
 end
