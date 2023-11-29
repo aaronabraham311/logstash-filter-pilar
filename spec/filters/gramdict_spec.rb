abb8d16c36843fea68eb7f00735245528db2283a
aaronabraham311 <aaronabraham311@gmail.com>
2023-11-29 17:45:52 +0100

    fix: move upload_grams_to_gram_dict from Preprocessor to GramDict

--- spec/filters/gramdict_spec.rb
+++ spec/filters/gramdict_spec.rb
@@ -66,4 +66,62 @@ describe GramDict do
         .to change { subject.instance_variable_get(:@four_gram_dict)[four_gram] }.from(1).to(2)
     end
   end
+
+  describe '#upload_grams' do
+    context 'with one token' do
+      let(:tokens) { ['token1'] }
+
+      it 'updates only the single gram dictionary' do
+        expect { subject.upload_grams(tokens) }
+          .to change { subject.single_gram_dict['token1'] }.from(nil).to(1)
+        expect(subject.double_gram_dict).to be_empty
+        expect(subject.tri_gram_dict).to be_empty
+      end
+    end
+
+    context 'with two tokens' do
+      let(:tokens) { %w[token1 token2] }
+      let(:double_gram) { 'token1^token2' }
+
+      it 'updates the single and double gram dictionaries' do
+        expect { subject.upload_grams(tokens) }
+          .to change { subject.single_gram_dict['token1'] }.from(nil).to(1)
+          .and change { subject.single_gram_dict['token2'] }.from(nil).to(1)
+          .and change { subject.double_gram_dict[double_gram] }.from(nil).to(1)
+        expect(subject.tri_gram_dict).to be_empty
+      end
+    end
+
+    context 'with three tokens' do
+      let(:tokens) { %w[token1 token2 token3] }
+      let(:double_gram1) { 'token1^token2' }
+      let(:double_gram2) { 'token2^token3' }
+      let(:tri_gram) { 'token1^token2^token3' }
+
+      it 'updates the single, double, and triple gram dictionaries' do
+        expect { subject.upload_grams(tokens) }
+          .to change { subject.single_gram_dict['token1'] }.from(nil).to(1)
+          .and change { subject.single_gram_dict['token2'] }.from(nil).to(1)
+          .and change { subject.single_gram_dict['token3'] }.from(nil).to(1)
+          .and change { subject.double_gram_dict[double_gram1] }.from(nil).to(1)
+          .and change { subject.double_gram_dict[double_gram2] }.from(nil).to(1)
+          .and change { subject.tri_gram_dict[tri_gram] }.from(nil).to(1)
+      end
+    end
+
+    context 'with an empty token array' do
+      let(:tokens) { [] }
+
+      it 'does not update any gram dictionaries' do
+        expect { subject.upload_grams(tokens) }
+          .not_to(change { subject.single_gram_dict })
+
+        expect { subject.upload_grams(tokens) }
+          .not_to(change { subject.double_gram_dict })
+
+        expect { subject.upload_grams(tokens) }
+          .not_to(change { subject.tri_gram_dict })
+      end
+    end
+  end
 end
