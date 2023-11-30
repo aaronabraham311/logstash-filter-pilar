9676bdfb101b1322893119246bc3a0307ae21f2f
Yash Dani <daniyash19@gmail.com>
2023-11-30 16:23:16 +0000

    added pilar tests

--- spec/filters/pilar_spec.rb
+++ spec/filters/pilar_spec.rb
@@ -4,4 +4,39 @@ require_relative '../spec_helper'
 require 'logstash/filters/pilar'
 
 describe LogStash::Filters::Pilar do
+  let(:config) { { 'source_field' => 'sample_log' } }
+  subject(:pilar_filter) { described_class.new(config) }
+
+  before do
+    pilar_filter.register
+  end
+
+  describe 'registration' do
+    it 'correctly register without errors' do
+      expect { pilar_filter }.not_to raise_error
+    end
+  end
+
+  describe 'filtering' do
+    sample_log = '- 1120928280 2005.07.09 R21-M0-NB-C:J05-U11 2005-07-09-09.58.00.188544 R21-M0-NB-C:J05-U11 ' \
+                 'RAS KERNEL INFO generating core.10299'
+
+    let(:event) { LogStash::Event.new('sample_log' => sample_log) }
+
+    before do
+      pilar_filter.filter(event)
+    end
+
+    it 'correctlys sets the event_string field' do
+      expect(event.get('event_string')).not_to be_nil
+    end
+
+    it 'correctly sets the template_string field' do
+      expect(event.get('template_string')).not_to be_nil
+    end
+
+    it 'correctly sets the raw_log field to the value of the source_field' do
+      expect(event.get('raw_log')).to eq(sample_log)
+    end
+  end
 end
