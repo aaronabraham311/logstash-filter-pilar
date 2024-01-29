d7ebf64475a807c9aebcd6995ccd3d8191dd2c33
aaronabraham311 <aaronabraham311@gmail.com>
2024-01-29 10:41:58 -0500

    feat: add parameterization variables

--- lib/logstash/filters/pilar.rb
+++ lib/logstash/filters/pilar.rb
@@ -13,19 +13,43 @@ module LogStash
       # Optional configuration: Specify the field name that contains the message to be used.
       # If this is not set, the filter will use the value of the "message" field by default.
       config :source_field, validate: :string, default: 'message'
+
+      # To improve accuracy of the parsing plugin, users will have the option of sending pre-existing logs
+      # which the parser will use to seed data structures. This seeding process will greatly improve accuracy
+      # of subsequent log parsing
       config :seed_logs_path, validate: :path, required: false
+
+      # The parsing algorithm requires a numeric probabilistic threshold to determine whether a
+      # particular parsed token is a dynamic token (i.e. changes extremely frequently) or if it is static.
+      # If the probability that the token is a dynamic token is above this threshold, the token is considered
+      # dynamic. The default threshold is set at 0.5. Since this is a probability threshold, the config value
+      # must be between 0 and 1.
+      config :dynamic_token_threshold, validate: :float, required: false, default: 0.5
+
+      # The standard log format for the application must be included in this plugin's configuration in the format
+      # of "<log_part_1_placeholder> <log_part_2_placeholder> ...". For example, if logs are usually of the form
+      # "02012024 1706542368 Random log", then the log format would be "<date> <time> <message>".
+      # If no log format is included, we will use the default of "<date> <time> <message>"
+      config :logformat, validate: :string, required: false, default: '<date> <time> <message>'
+
+      # The content_specifier variable is the placeholder value in the `logformat` variable which the parser should use
+      # to identify the actual log message. For example, if `logformat = '<date> <time> <message>'`, then the
+      # content_specifier should be 'message' since this is the part of the log that the parser should parse. The
+      # default will be 'message', matching the default format in the `logformat` variable
+      config :content_specifier, validate: :string, required: false, default: 'message'
 
       def register
         @gramdict = GramDict.new
         @preprocessor = Preprocessor.new(@gramdict, '<date> <time> <message>', 'message')
 
-        # populate gramdict with seed logs 
-        if @seed_logs_path 
-          ::File.open(@seed_logs_path, "r") do |seed_logs|
-            seed_logs.each_line do |seed_log|
-              # TODO: Here, we are parsing every seed log file when we don't need to, might need to separate these steps out
-              @preprocessor.process_log_event(seed_log)
-            end
+        # populate gramdict with seed logs
+        return unless @seed_logs_path
+
+        ::File.open(@seed_logs_path, 'r') do |seed_logs|
+          seed_logs.each_line do |seed_log|
+            # TODO: Here, we are parsing every seed log file when we don't need to,
+            # might need to separate these steps out
+            @preprocessor.process_log_event(seed_log)
           end
         end
       end
